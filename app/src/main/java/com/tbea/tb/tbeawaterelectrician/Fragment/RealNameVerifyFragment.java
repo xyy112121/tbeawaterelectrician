1b1d0d60eda96aa019564e5acf9638de91a026c9
mac13 <47007209@qq.com>
2016-12-16 08:56:47 +0800

    注册页面

---
+++ app/src/main/java/com/tbea/tb/tbeawaterelectrician/Fragment/RealNameVerifyFragment.java
@@ -0,0 +1,21 @@
+package com.tbea.tb.tbeawaterelectrician.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tbea.tb.tbeawaterelectrician.R;
+
+/**
+ * Created by abc on 16/12/15. 实名认证注册
+ */
+
+public class RealNameVerifyFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = (View)inflater.inflate(R.layout.fragment_register2,null);
+        return view;
+    }
+}
