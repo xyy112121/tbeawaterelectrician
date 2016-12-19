e2e27b1866af13d58d710de6791e6ca30f38ff98
mac13 <47007209@qq.com>
2016-12-19 08:44:25 +0800

    首页

--- app/src/main/java/com/tbea/tb/tbeawaterelectrician/activity/MainActivity.java
+++ app/src/main/java/com/tbea/tb/tbeawaterelectrician/activity/MainActivity.java
@@ -1,13 +1,17 @@
 package com.tbea.tb.tbeawaterelectrician.activity;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.tbea.tb.tbeawaterelectrician.R;
 import com.tbea.tb.tbeawaterelectrician.component.MainNavigateTabBar;
 import com.tbea.tb.tbeawaterelectrician.fragment.HomeFragment;
+import com.tbea.tb.tbeawaterelectrician.fragment.MyFragment;
+import com.tbea.tb.tbeawaterelectrician.fragment.TakeFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
     private MainNavigateTabBar mNavigateTabBar;
 
     @Override
@@ -21,13 +25,21 @@ public class MainActivity extends AppCompatActivity {
         mNavigateTabBar.addTab(HomeFragment.class, new MainNavigateTabBar.TabParam(R.drawable.icon_home, R.drawable.icon_home_select, "首页"));
         mNavigateTabBar.addTab(HomeFragment.class, new MainNavigateTabBar.TabParam(R.drawable.icon_nearby, R.drawable.icon_nearby_select, "附近"));
         mNavigateTabBar.addTab(HomeFragment.class, new MainNavigateTabBar.TabParam(0, 0, ""));
-        mNavigateTabBar.addTab(HomeFragment.class, new MainNavigateTabBar.TabParam(R.drawable.icon_then_live, R.drawable.icon_then_live_select, "接活"));
-        mNavigateTabBar.addTab(HomeFragment.class, new MainNavigateTabBar.TabParam(R.drawable.icon_my, R.drawable.icon_my_select, "我"));
+        mNavigateTabBar.addTab(TakeFragment.class, new MainNavigateTabBar.TabParam(R.drawable.icon_then_live, R.drawable.icon_then_live_select, "接活"));
+        mNavigateTabBar.addTab(MyFragment.class, new MainNavigateTabBar.TabParam(R.drawable.icon_my, R.drawable.icon_my_select, "我"));
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mNavigateTabBar.onSaveInstanceState(outState);
+    }
+
+    public void setTopGone(){
+        findViewById(R.id.mian_top).setVisibility(View.GONE);
+    }
+
+    public void setTopShow(){
+        findViewById(R.id.mian_top).setVisibility(View.VISIBLE);
     }
 }
